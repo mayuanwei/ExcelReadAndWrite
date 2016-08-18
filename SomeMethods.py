69d43da146e4834ab914afd2872342961317ce51
mayuanwei <ma_yuan_wei@163.com>
2016-08-18 22:18:27 +0800

    team()

--- SomeMethods.py
+++ SomeMethods.py
@@ -26,6 +26,28 @@ def convert_Camel_to_low_underline_by_regular(str):
 
     return str.lower()
 
+#2队比赛，分别有队员a,b,c和x,y,z，已知a和x不比赛，c和x、z不比赛，求对阵情况
+def team():
+    team1 = ['a', 'b', 'c']
+    team2 = ['x', 'y', 'z']
+
+    team3 = []
+    team4 = []
+    for t1 in team1:
+        if t1=='c':
+            for t2 in team2:
+                if(t2!='x' and t2!='z'):print(t1,t2)
+                else:team4.append(t2)
+        else:team3.append(t1)
+
+    for t3 in team3:
+        if t3=='a':
+            for t4 in team4:
+                if t4!='x':print(t3,t4)
+                else:t5 = t4
+        else:print(t3,t5)
+
 if __name__ == '__main__':
-    print(convert_Camel_to_low_underline('MyNameIs'))
-    print(convert_Camel_to_low_underline_by_regular('MyNameIs'))
+    #print(convert_Camel_to_low_underline('MyNameIs'))
+    #print(convert_Camel_to_low_underline_by_regular('MyNameIs'))
+    team()
